7e4fdfeca364b160dcc5f3835ca47c6a23abd0ac
tee29909 <59160571@go.buu.ac.th>
2019-08-12 13:56:17 +0700

    random

--- app/src/main/java/buu/informatics/s59160571/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160571/diceroller/MainActivity.kt
@@ -3,18 +3,28 @@ package buu.informatics.s59160571.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val resultText: TextView = findViewById(R.id.result_text)
         val rollButton: Button = findViewById(R.id.roll_button)
+        resultText.text = "Dice Rolled"
         rollButton.setOnClickListener { rollDice() }
+
+
 
     }
     private  fun rollDice(){
+        val randomInt = Random().nextInt(6)+1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+
         Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
     }
 }
