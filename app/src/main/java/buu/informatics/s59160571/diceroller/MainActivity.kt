2e2e4ad108f11e7b51b2ab8d6e0b7b0a24dc1970
tee29909 <59160571@go.buu.ac.th>
2019-08-12 14:56:02 +0700

    Image

--- app/src/main/java/buu/informatics/s59160571/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160571/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package buu.informatics.s59160571.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+
 import android.widget.Toast
 import java.util.*
 
@@ -12,11 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val resultText: TextView = findViewById(R.id.result_text)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         val resetButton: Button = findViewById(R.id.reset_button)
 
-        resultText.text = "Dice Rolled"
+
         rollButton.setOnClickListener { rollDice() }
         resetButton.setOnClickListener { resetDice() }
 
@@ -25,15 +26,25 @@ class MainActivity : AppCompatActivity() {
     }
     private  fun rollDice(){
         val randomInt = Random().nextInt(6)+1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImge: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImge.setImageResource(drawableResource)
 
-        Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
+
+
+
     }
     private  fun resetDice(){
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
+
+
 
         Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
     }
